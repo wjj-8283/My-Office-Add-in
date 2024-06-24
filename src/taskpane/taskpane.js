32940595bf246ce4165aa32ee47c40fe7e51d6d8
Sb <wlw20070926@outlook.com>
2024-06-24 14:06:29 +0800

    gjfgjfgjhfgjfgjh

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -11,7 +11,7 @@ export async function run() {
 
     const paragraph = context.document.body.insertParagraph("bhd获得合法合规和雕塑覅呼呼大睡复古很好的李的飞机设计和发动机客户建国后dhf", Word.InsertLocation.end);
     const wjj = context.document.body.insertParagraph("jk搞撒低级刚才发图茂标的符合公司的开发部署", Word.InsertLocation.end);
-    const wff = context.document.body.insertParagraph("cb书宽说的话ui公司的林会计粉红色呃u匮乏昏聩的覅u是乎都是v福冈大阪dghdhg", Word.InsertLocation.end);
+    const wff = context.document.body.insertParagraph("cb书宽说的gfjvjvhbjv粉红色呃u匮乏昏聩的覅u是乎都是v福冈大阪dghdhg", Word.InsertLocation.end);
     const wkk = context.document.body.insertParagraph("sd看guts这个副sb业阿飞故园附庸国df", Word.InsertLocation.end);
 
     paragraph.font.color = "green";
