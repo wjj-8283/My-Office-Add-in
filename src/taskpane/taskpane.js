5d7e59ba1699434b951e2901a00c75d855f849b1
wjj-8283 <wlw20070926@outlook.com>
2024-09-06 10:45:00 +0800

    rfuhtgtufui

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -9,14 +9,14 @@ Office.onReady((info) => {
 export async function run() {
   return Word.run(async (context) => {
 
-    const paragraph = context.document.body.insertParagraph("bhd获得合很好的bvdgfhg的飞机设计和发动机客户建国后dhf", Word.InsertLocation.end);
-    const wjj = context.document.body.insertParagraph("jk搞fsdjhfjfysdjuyf司的开发部署", Word.InsertLocation.end);
-    const wff = context.document.body.insertParagraph("cb书宽说的gftytytjvgfhjkhgjkdfhgkjdf昏聩的覅u是乎都是v福冈大阪dghdhg", Word.InsertLocation.end);
-    const wkk = context.document.body.insertParagraph("sd看gutggujfuftys这个jkhjkui庸国df", Word.InsertLocation.end);
+    const paragraph = context.document.body.insertParagraph("bhdghrfufuyjfyju的bvdgfhg的飞机设计和发动机客户建国后dhf", Word.InsertLocation.end);
+    const wjj = context.document.body.insertParagraph("jk搞fsdjhfjfysdj开jklhjkh发部署", Word.InsertLocation.end);
+    const wff = context.document.body.insertParagraph("cb书宽说的gftytytjkdfhguioyhuiyiokjdf昏聩的覅u是乎都是v福冈大阪dghdhg", Word.InsertLocation.end);
+    const wkk = context.document.body.insertParagraph("sd看gutggujfuftyuiyhiugs这个7807kui庸国df", Word.InsertLocation.end);
 
-    paragraph.font.color = "green";
+    paragraph.font.color = "orange";
     wjj.font.color = "pink";
-    wff.font.color = "yellow";
+    wff.font.color = "red";
     wkk.font.color = "blue";
     await context.sync();
   });
