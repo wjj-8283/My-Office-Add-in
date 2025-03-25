074ef461a8c476ba8a7454e471c4d9e07bf610cf
wjj-8283 <wlw20070926@outlook.com>
2025-03-25 18:19:43 +0800

    好

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -10,9 +10,9 @@ export async function run() {
   return Word.run(async (context) => {
 
     const paragraph = context.document.body.insertParagraph("gsjuf机设计和发动机客户建国后dhf", Word.InsertLocation.end);
-    const wjj = context.document.body.insertParagraph("jk搞fsjkdehuiughierdjhfgsgdddfhjkusxhfjjussdhhdgdhjdsjklhjkh发部署", Word.InsertLocation.end);
-    const wff = context.document.body.insertParagraph("cb书gftyfygukfdufdtu运动发育东方影都都是浮云大阪dghdhg", Word.InsertLocation.end);
-    const wkk = context.document.body.insertParagraph("sd看gutftshsjfghjksdhfdsjkhfyuitgf7kui庸国jf是对u士大夫以上的覅是对uihguisddf", Word.InsertLocation.end);
+    const wjj = context.document.body.insertParagraph("jk搞fsjkdehuiughi规划风格化风格化风格化jklhjkh发部署", Word.InsertLocation.end);
+    const wff = context.document.body.insertParagraph("cb书gftyfygukfdu获得解放和大家开会hdhg", Word.InsertLocation.end);
+    const wkk = context.document.body.insertParagraph("sd看gutftshsjfg是对uihguisddf", Word.InsertLocation.end);
 
     paragraph.font.color = "purple";
     wjj.font.color = "pink";
