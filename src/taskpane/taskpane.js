589b3121506dfb7273fb9420f291e186de500c6b
wjj-8283 <wlw20070926@outlook.com>
2025-06-25 08:50:27 +0800

    feat: 修改一些拼写错误

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -9,9 +9,9 @@ Office.onReady((info) => {
 export async function run() {
   return Word.run(async (context) => {
 
-    const paragraph = context.document.body.insertParagraph("gsjufsdhudsfgfjfuifds发国后dhf", Word.InsertLocation.end);
-    const wjj = context.document.body.insertParagraph("jk搞fsi规划风格jjyfjytyklhjkh发部署", Word.InsertLocation.end);
-    const wkk = context.document.body.insertParagraph("sd看esduisddf", Word.InsertLocation.end);
+    const paragraph = context.document.body.insertParagraph("gsjufkdhkshdkf国后dhf", Word.InsertLocation.end);
+    const wjj = context.document.body.insertParagraph("jk搞fsi规shukhlhjkh发部署", Word.InsertLocation.end);
+    const wkk = context.document.body.insertParagraph("sd看esddf", Word.InsertLocation.end);
 
     paragraph.font.color = "red";
     wjj.font.color = "pink";
