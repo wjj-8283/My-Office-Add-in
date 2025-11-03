d4a3221814278e2dcc93ebc5678887158adf2e5d
wjj <82750345+wjj-8283@users.noreply.github.com>
2025-11-03 14:51:38 +0800

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global global, Office, self, window */
-
 Office.onReady(() => {
   // If needed, Office.js is ready to be called
 });
