e70c28e9596795aeaf806db81b1030f2c46e7761
wjj-8283 <wlw20070926@outlook.com>
2026-02-24 12:22:46 +0800

    移除奇怪的空白行

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -5,30 +5,12 @@ Office.onReady((info) => {
     document.getElementById("run").onclick = run;
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export async function run() {
   return Word.run(async (context) => {
 
     const paragraph = context.document.body.insertParagraph("gsjufkdhksakdyuiasydasdicjcififif后辛庄村国后dhf", Word.InsertLocation.end);
-    const wjj = context.document.body.insertParagraph("jk搞fsi规说好的撒娇jkhifififufudf发部署", Word.InsertLocation.end);
-    const wkk = context.document.body.insertParagraph("jk搞fsi规说lshjfdihjsdiofdsf发部署", Word.InsertLocation.end);
+    const wjj = context.document.body.insertParagraph("jk搞ffuiygassuadhkifufudf发部署", Word.InsertLocation.end);
+    const wkk = context.document.body.insertParagraph("jk搞fsdiahdadsdiofdsf发部署", Word.InsertLocation.end);
 
     paragraph.font.color = "red";
     wjj.font.color = "pink";
